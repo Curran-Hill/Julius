9610119c29aaf1ba8ac056321012a1c6776c0081
Curran-Hill <currankhill@gmail.com>
2025-09-07 20:16:50 -0500

    modified to follow best practices included removing repeated type casting and cleaner input validation. Also atempted some future proofing with having encode() return ciphertext instead of directly printing in the function.

--- Affine Cipher/affine.py
+++ Affine Cipher/affine.py
@@ -2,15 +2,21 @@ def encode():
 
     plaintext = input("Paste plain text here: ")
 
-    a = input("A: ")
-    while a.isdigit() == False or int(a) % 2 == 0 or int(a) == 13 or int(a) not in range(1,26):
-        print('ERROR! Invalid input for "A"')
-        a = input("Please retry: ")
+    while True:
+        a = input("A: ")
+        if a.isdigit() and int(a) % 2 != 0 and int(a) != 13 and int(a) in range(1, 26):
+            a = int(a)
+            break
+        else:
+            print('ERROR! Invalid input for "A"')
             
-    b = input("B: ")
-    while b.isdigit() == False or int(b) not in range(1,26):
-        print('ERROR! Invalid input for "B"')
-        b = input("Please retry: ")
+    while True:
+        b = input("B: ")
+        if b.isdigit() and int(b) % 2 != 0 and int(b) != 13 and int(b) in range(1, 26):
+            b = int(b)
+            break
+        else:
+            print('ERROR! Invalid input for "B"')
 
     x_arr = []
 
@@ -20,18 +26,18 @@ def encode():
     ciphertext = ""
 
     for x in x_arr:
-        if x in range(97,123): #Unicode range for lowercase letters
-            x = x-ord('a')%26
-            new_char = (int(a)*x+int(b))%26
+        if x in range(ord('a'),ord('z')+1):
+            x = (x-ord('a')) % 26
+            new_char = (a * x + b) % 26
             ciphertext += chr(int(new_char)+ord('a'))
-        elif x in range(65,90): #Unicode range for uppercase letters
-            x = x-ord('A')%26
-            new_char = (int(a)*x+int(b))%26
-            ciphertext += chr(int(new_char)+ord('A'))
+        elif x in range(ord('A'),ord('Z') + 1):
+            x = (x-ord('A')) % 26
+            new_char = (a * x + b) % 26
+            ciphertext += chr(int(new_char) + ord('A'))
         else:
             new_char = chr(x)
             ciphertext += new_char
         
-    print(ciphertext)
+    return ciphertext
 
-encode()
+print(encode())
